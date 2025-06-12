63a639e20d2fbfec2cae8c2e680ee16682dc6103
mayur wagh <mswagh98@gmail.com>
2025-06-12 17:03:06 +0530

    refactor(auth): split auth controller into separate modules
    Move register, getMe, updateDetails and forgotPassword handlers into individual controller files to improve code organization and maintainability

---
+++ backend/controller/profile.controller.js
@@ -0,0 +1,11 @@
+const User = require("../models/User");
+const asyncHandler = require("../middleware/async");
+
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
