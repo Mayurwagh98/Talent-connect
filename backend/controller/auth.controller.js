4767f29e20563497b6005be68aafd5d860925c76
mayur wagh <mswagh98@gmail.com>
2025-06-12 17:04:42 +0530

    refactor(auth): move logout and resetPassword handlers to separate files
    Split auth controller into smaller modules by extracting logout and resetPassword functionality into dedicated controller files to improve code organization and maintainability

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -4,51 +4,6 @@ const asyncHandler = require("../middleware/async");
 // const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
 
-
-
-// @desc    Reset password
-// @route   PUT /api/auth/resetpassword/:resettoken
-// @access  Public
-exports.resetPassword = asyncHandler(async (req, res, next) => {
-  // Get hashed token
-  const resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(req.params.resettoken)
-    .digest("hex");
-
-  const user = await User.findOne({
-    resetPasswordToken,
-    resetPasswordExpire: { $gt: Date.now() },
-  });
-
-  if (!user) {
-    return next(new ErrorResponse("Invalid token", 400));
-  }
-
-  // Set new password
-  user.password = req.body.password;
-  user.resetPasswordToken = undefined;
-  user.resetPasswordExpire = undefined;
-  await user.save();
-
-  sendTokenResponse(user, 200, res);
-});
-
-// @desc    Log user out / clear cookie
-// @route   GET /api/auth/logout
-// @access  Private
-exports.logout = asyncHandler(async (req, res, next) => {
-  res.cookie("token", "none", {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
-
-  res.status(200).json({
-    success: true,
-    data: {},
-  });
-});
-
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
---
+++ backend/controller/logout.controller.js
@@ -0,0 +1,13 @@
+const asyncHandler = require("../middleware/async");
+
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
